81f25aebe4f6c56e6becf0f7c59511ec023edff4
Nicholas Griffiths <nichg999@gmail.com>
2025-01-22 18:54:43 -0700

    practicing var

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var name = "Nick";
+/* var name = "Nick";
 var age = 26;
 
 var message = "Hi, my name is " + name + " and I am " + age + " years old.";
         
 //alert(message);
 
-console.log(message);
+console.log(message); */
+
+//Variables in JavaScript
+
+var userName = "itznich";
+var totalWins = 30;
+var totalLosses = 10;
+
+/* Variable Rules */
+
+// Should have a meaningful name
+// Cannot contain hyphen (-) or space
+// Cannot start with a number
+// Cannot use reserved keywords
+// Variables are case sensitive
+// Should be camelCase
+
+console.log(userName);
