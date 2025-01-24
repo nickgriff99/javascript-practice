176f8a6e1abf4c664ab74744e7f8d714a4a66a70
Nicholas Griffiths <nichg999@gmail.com>
2025-01-24 00:33:14 -0700

    formatting if statements to make them more readable

--- index.js
+++ index.js
@@ -193,9 +193,15 @@ console.log("Final Price: $" + finalPrice.toFixed(2));
 var skater1 = 9.5;
 var skater2 = 9;
 var skater3 = 9;
+var isJackedUpOnCaffeine = true;
 
 if (skater2 > skater3 && skater2 <= skater1 && skater1 < 100) {
   console.log('Move on to the next round!');
-} else if (skater3 > skater2 || skater3 === skater2 && skater2 < skater1) {
-  console.log('skater3 is steppin up their game!');
+} else if (skater3 > skater2 || skater3 === skater2 
+  && skater2 < skater1 
+  && !isJackedUpOnCaffeine
+) {
+  console.log('skater3 is stepping up their game!');
+} else if (skater3 > 5 && isJackedUpOnCaffeine) {
+  console.log('skater3 is on fire! gold for days!');
 }
