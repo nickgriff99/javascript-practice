54f05a940ba7d63a847809650b439cea1f2a71ef
Nicholas Griffiths <nichg999@gmail.com>
2025-01-24 21:08:27 -0700

    practicing debugging

--- index.js
+++ index.js
@@ -222,3 +222,182 @@ if (currentLightColor === 'red') {
 }
 
 /* END OF JAVASCRIPT PRACTICE 4 */
+
+/* ----------------------- */
+
+// JavaScript Debugging Practice
+
+// Part 1
+
+/*
+Directions:
+1. Fix syntax errors.
+2. Run the following code as-is and read the error message.
+3. Fix the mistake, and then re-run the code to check it.
+
+Expected Output:
+=> WARNING: Insufficient energy!
+*/
+
+let readyToSkate = false;
+let energyLevel = 18000;
+
+if (energyLevel >= 24000) {
+   console.log("Energy level is a go!");
+   readyToSkate = true;
+} else {
+   console.log("WARNING: Insufficient energy!");
+   readyToSkate = false;
+}
+
+// Part 2
+
+/*
+Directions:
+1. The block of code has a few syntax errors.
+2. Run the code as-is to find the mistakes.
+
+💡 Tip:
+Only one error will be flagged at a time.
+Fix that ONE problem, and then re-run the code to check your work.
+Avoid trying to fix multiple issues at once.
+
+Expected Output:
+=> Crew & cameraBattery is a go!
+=> Grab your skateboard...
+=> Ready...
+=> Set...
+=> SKATE!
+*/
+
+readyToSkate = false;
+let crewStatus = true;
+let cameraBattery = "green";
+
+if (crewStatus && cameraBattery === "green") {
+   console.log("Crew & cameraBattery is a go!");
+   readyToSkate = true;
+} else {
+   console.log("WARNING: Crew or cameraBattery not ready!");
+   readyToSkate = false;
+}
+
+if (readyToSkate) {
+   console.log("Grab your skateboard...");
+   console.log("Ready...");
+   console.log("Set...");
+   console.log("SKATE!");
+} else {
+   console.log("No skating today.");
+}
+
+// Part 3
+
+/*
+Directions:
+1. Fix runtime errors.
+2. Remember to examine the error message for clues about what is going wrong.
+3. Pay close attention to any line numbers mentioned in the message - these will help you locate and fix the mistake in the code
+
+Expected Result:
+=> WARNING: Insufficient energy!
+*/
+
+readyToSkate = false;
+energyLevel = 18000;
+
+if (energyLevel >= 21000) {
+  console.log("Energy level is a go!");
+  readyToSkate = true;
+} else {
+  console.log("WARNING: Insufficient energy!");
+  readyToSkate = false;
+}
+
+// Part 4
+
+/*
+Directions:
+1. Find and fix the runtime error.
+
+Expected Result:
+=> Energy level is a go!
+=> Grab your skateboard...
+=> Ready...
+=> Set...
+=> SKATE!
+*/
+
+readyToSkate = false;
+energyLevel = 24010;
+
+if (energyLevel >= 24000) {
+  console.log("Energy level is a go!");
+  readyToSkate = true;
+} else {
+  console.log("WARNING: Insufficient energy!");
+  readyToSkate = false;
+}
+
+if (readyToSkate) {
+  console.log("Grab your skateboard...");
+  console.log("Ready...");
+  console.log("Set...");
+  console.log("SKATE!");
+} else {
+   console.log("No skating today.");
+}
+
+// Part 5
+
+/*
+Requirements:
+1. It's time to skate only if your energy, crew and camera battery all check out OK.
+2. If a check fails, print that information to the console and forget about skating today.
+3. If all checks are successful, print a message to the console and go skate!
+
+Logic errors do not generate warning messages or prevent the code from running, but the program still does not work as intended.
+First, run this sample code as-is and examine the output. Should you be ready to skate? Did you?
+
+💡 Tips:
+1. Break the code down into smaller chunks.
+2. Consider the first if/else block. Add console.log(readyToSkate) after this block, then run the program. Given the energyLevel value, should readyToSkate be true or false after the check? Is the program behaving as expected?
+3. Consider the second if/else block. Add another console.log(readyToSkate) after this block and run the program. Given crewStatus and cameraStatus, should readyToSkate be true or false after this check?
+4. Consider both if/else blocks together. Given the values for energyLevel, crewStatus and cameraStatus, should readyToSkate be true or false per the requirements? Is the program behaving as expected?
+5. When checking the energy readiness and the crew & camera readiness, does it make sense to store the result to a single variable, readyToSkate? Perhaps energy readiness and crew & camera readiness should have their own variable to update 🤷‍♂️
+
+Expected Result:
+=> WARNING: Insufficient energy!
+=> Crew & cameraBattery is a go!
+=> No skating today.
+*/
+
+readyToSkate = false;
+energyLevel = 18000;
+crewStatus = true;
+cameraBattery = "green";
+
+if (energyLevel >= 24000) {
+   console.log("Energy level is a go!");
+} else {
+   console.log("WARNING: Insufficient energy!");
+   readyToSkate = false;
+}
+
+if (crewStatus && cameraBattery === "green"){
+   console.log("Crew & cameraBattery is a go!");
+} else {
+   console.log("WARNING: Crew or cameraBattery not ready!");
+   readyToSkate = false;
+}
+
+if (readyToSkate) {
+   console.log("Let's goooooo!");
+   console.log("Time to skate!");
+} else {
+   console.log("No skating today.");
+}
+
+/* END OF JAVASCRIPT DEBUGGING PRACTICE */
+
+/* ----------------------- */
